eb0fad01530a3addfa377cfcbf7517356f040bb3
林宥丞 <a26913700@gmail.com>
2018-11-27 12:43:47 +0800

    練習 2-1：修改 Route 接受參數

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,13 @@
 Route::get('/', function() {
     return view('welcome');
 });
-*/
+
 
 Route::get('/', function() {
     return redirect('welcome');
+});
+*/
+
+Route::get('hello/{name}', function($name) {
+    return 'Hello, '.$name;
 });
